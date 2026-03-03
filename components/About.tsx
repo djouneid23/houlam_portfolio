5cb07cdc932338ee4fd777e8fbafbbd4fcf525c9
djouneid23 <tess.djouneid@gmail.com>
2026-03-03 13:40:32 +0300

    Modification du contenu

--- components/About.tsx
+++ components/About.tsx
@@ -7,15 +7,17 @@ export default function About() {
           Ingénieur orineté architecture des systèmes d&apos;information, sécurité avancée et intellignece artificielle, je combine une expertise 
           en Génie Logiciel et Cybersécurité avec intelligence Artificiel.
         </p>
-        
         <div className="space-y-3 text-slate-300">
               Mon approche repose sur trois piliers
-              <p><span className="text-cyan-400">Conception de system d'information performants</span></p>
+              <p><span className="text-cyan-400">Conception de system d&apos;information performants</span></p>
               <p><span className="text-cyan-400">Sécurisations avacés des infrastructures numérique</span></p>
               <p><span className="text-cyan-400">Inteligence artificielle appliquée à la détection et à l&apos;analyse des menaces</span></p>
         </div>
         <p className="text-slate-400 leading-relaxed">
-          Je construit des solutions technologiques qui allient performances, sécurité et int
+          Je construit des solutions technologiques qui allient performances, sécurité et intelligence décisionnelle.
+        </p>
+        <p className="text-slate-400 leading-relaxed">
+          Mon ambition est de devenir une référence en conception de systèmes sécurisés et intelligents au niveau national et internationnal 
         </p>
       </div>
     </section>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,5 @@
+import { title } from "process";
+
 // =============================
 export default function Projects() {
   const projects = [
@@ -11,11 +13,36 @@ export default function Projects() {
       <div className="max-w-6xl mx-auto px-6">
         <h3 className="text-3xl font-bold mb-12 text-center">Projets</h3>
         <div className="grid md:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
+          {[
+            {
+              title: "Creation d'une application mobile",
+              desc: "Creation d'une application mobile et Web de vente des medicaments en ligne et localisation des pharmacies"
+            },
+            {
+              title: "Création d'un HomeLab",
+              desc: "Création d'un HomLab pour mes travaux en SOC, SIEM et Test d'Intrusion sous Proxmox avec un accées VPN en cas de deplacement"
+            },
+            {
+              title: "Architecture de sécurité",
+              desc: "Création d'un prototype de sécurité pour les entreprises"
+            },
+            {
+              title: "Mini SOC Intelligente",
+              desc : "Mise en place d'un mini SOC inteligente avec suricata comme IDS/IPS, wazuh et IA pour reduire les faux positifs"
+            },
+            {
+              title: "Agent IA",
+              desc: "Création d'un agent IA en react et nodejs et l'API opnrouter qui permet de discuter avec et lui poser des questions il traite seulement les texts mais pas les fichiers"
+            },
+            {
+              title : "Lab Réseau",
+              desc: "Création d'un Lab réseau qui permet de relier plusieur site ou entreprise sous GNS3 en utilisant fortigate(VPN Hub-and-Spock) "
+            }
+          ].map((project, index) => (
             <div key={index} className="bg-slate-800 p-8 rounded-2xl shadow-lg hover:scale-105 transition-transform">
-              <h4 className="text-xl font-semibold mb-4">{project}</h4>
+              <h4 className="text-xl font-semibold mb-4">{project.title}</h4>
               <p className="text-slate-400">
-                Description technique détaillée, architecture sécurisée et stack utilisée.
+                {project.desc}
               </p>
             </div>
           ))}
